958db6768da182d0a387e494b791316bc2b39cf1
Nisharu <nisharu99@gmail.com>
2023-09-12 18:58:40 +0530

    Signed-off-by: Nisharu <nisharu99@gmail.com>

--- app/Http/Livewire/ConfirmPage.php
+++ app/Http/Livewire/ConfirmPage.php
@@ -10,7 +10,6 @@ class ConfirmPage extends Component
     public function render()
     {
         $data = session()->get('dispatchDetails');
-        // dd($data);
         return view('livewire.confirm-page',compact('data'))->layout('layouts.front_master');
     }
 }
